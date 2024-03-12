ce8b2148c2491da90bbb904f982dfe6291c63c7a
Kartik Deshpande <kartikdeshpande@myyahoo.com>
2024-03-12 15:11:41 +0000

    Update

--- Page View Time Visualizer/time_series_visualizer.py
+++ Page View Time Visualizer/time_series_visualizer.py
@@ -1,3 +1,7 @@
+# scipy 1.7.3
+# numpy 1.18.0
+# matplotlib 3.2.1
+# pandas 1.3.4
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -5,7 +9,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('fcc-forum-pageviews.csv', index_col="date", parse_dates=True)
+df = pd.read_csv('/workspace/boilerplate-page-view-time-series-visualizer/fcc-forum-pageviews.csv', index_col="date", parse_dates=True)
 
 # Clean data
 
@@ -77,3 +81,29 @@ def draw_box_plot():
 
 
 
+
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('box_plot.png')
+    return fig
